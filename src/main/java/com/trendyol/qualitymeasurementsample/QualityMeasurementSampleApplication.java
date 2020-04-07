16a016d22e3f0f4a7300a72fa2615532014fca5c
Bahadir Tasdemir <bahadir.tasdemir@trendyol.com>
2020-04-07 16:42:07 +0300

    first commit

---
+++ src/main/java/com/trendyol/qualitymeasurementsample/QualityMeasurementSampleApplication.java
@@ -0,0 +1,13 @@
+package com.trendyol.qualitymeasurementsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualityMeasurementSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualityMeasurementSampleApplication.class, args);
+	}
+
+}
